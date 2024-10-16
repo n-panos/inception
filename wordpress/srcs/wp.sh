6f900376e1d26e267846671cf7f6068a995fcab6
Nacho <nacho@MacBook-Pro-de-Nacho.local>
2024-10-16 11:20:22 +0200

    wordpress re

--- wordpress/srcs/wp.sh
+++ wordpress/srcs/wp.sh
@@ -1,9 +1,8 @@
-cd /var/www/html/wordpress
-wp core download --path="/var/www/html/wordpress" --allow-root
-wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_USERPASS --dbhost=$DB_HOSTNAME --dbprefix=wp_
-wp user create --path="var/www/html/wordpress" --allow-root $WORDPRESS_DB_USER $WORDPRESS_DB_USER_EMAIL --user_pass=$WORDPRESS_DB_USER_PASSWORD
-wp theme install $WORDPRESS_THEME --activate --allow-root
-wp plugin update --allow-root --all
-wp post create --path="/var/www/html/wordpress" --allow-root --post_type=post --post_title='Inception here!' --post_status=publish
-mkdir -p /run/php/
-php-fpm7.3 -F
+docker-php-entrypoint php-fpm &
+until wp core install --url='http://localhost' --title=$WORDPRESS_DB_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL; do
+echo 'Trying to install WordPress...'
+sleep 5
+done;
+wp theme install $WORDPRESS_THEME --activate &&
+wp user create newuser $WORDPRESS_DB_USER --role=author --user_pass=$WORDPRESS_DB_USER_PASSWORD
+php-fpm --nodaemonize
