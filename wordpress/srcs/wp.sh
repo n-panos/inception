44cf4db72d3e6f3ce95df3af257742577f51d322
Nacho <nacho@MacBook-Pro-de-Nacho.local>
2024-10-22 12:17:46 +0200

    config.php

--- wordpress/srcs/wp.sh
+++ wordpress/srcs/wp.sh
@@ -1,5 +1,5 @@
 cd /var/www/html/wordpress
-wp core download  --path="/var/www/html/wordpress" --allow-root
+#wp core download  --path="/var/www/html/wordpress" --allow-root
 wp config create --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_USERPASS --dbhost=$DB_HOSTNAME --dbprefix=wp_
 wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN --title="$WORDPRESS_DB_TITLE" --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL
 wp plugin update --allow-root --all
