fd08d2f505acba7e755fe02c08f177faac26ebf6
Nacho <nacho@MacBook-Pro-de-Nacho.local>
2024-10-28 11:53:05 +0100

    php-fpm

--- wordpress/srcs/wp.sh
+++ wordpress/srcs/wp.sh
@@ -4,7 +4,7 @@ wp config create --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$
 wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN --title="$WORDPRESS_DB_TITLE" --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL
 wp plugin update --allow-root --all
 wp user create --path="/var/www/html/wordpress" --allow-root $WORDPRESS_DB_USER $WORDPRESS_DB_USER_EMAIL --user_pass=$WORDPRESS_DB_USER_PASSWORD
-wp theme install $WORDPRESS_THEME --activate --allow-root
+#wp theme install $WORDPRESS_THEME --activate --allow-root
 wp plugin update --allow-root --all
 wp post create --path="/var/www/html/wordpress" --allow-root --post_type=post --post_title='Testing inception!' --post_status=publish
 wp option update --path="/var/www/html/wordpress" --allow-root blogdescription 'IPANOS test'
